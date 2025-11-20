15cace59c72fdd11b39524dbe21a11311a6bd336
ZUYYINA AMALIA <zuyyinaamalia@students.undip.ac.id>
2025-11-20 10:42:30 +0700

    First Commit

---
+++ frontend/vuejs/frontend/src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { api } from '../services/api'
+import { ref, onMounted } from 'vue'
+
+const message = ref('')
+
+onMounted(async () => {
+  const res = await api.get('/test')
+  message.value = res.data
+})
+</script>
+
+<template>
+  <div>
+    <h1>Data dari Backend:</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
